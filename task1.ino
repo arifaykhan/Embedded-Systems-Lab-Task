c6bc7e59322f8e9fe124610167124cc754c0456d
Arif Aykhan Mammadli <79666195+arifaykhan@users.noreply.github.com>
2026-03-20 11:51:07 +0400

    Update task1.ino

--- task1.ino
+++ task1.ino
@@ -3,15 +3,15 @@ void setup() {
 
   // Timer/Counter 1
   TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << WGM11);
-  TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS10); // CS10 = No Prescaler
+  TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS10); // no prescaler
   
-  ICR1 = 1;    // The smallest possible TOP value (Frequency = 16MHz / (1 * (1+1)))
-  OCR1A = 1;   // 50% Duty Cycle
-  OCR1B = 1;   // 50% Duty Cycle
+  ICR1 = 1;    // the smallest possible TOP value (freq = 16MHz / (1 * (1+1)))
+  OCR1A = 1;   // 50% duty cycle
+  OCR1B = 1;   // 50% duty cycle
 
   // Timer/Counter2
-  TCCR2A = (1 << COM2A0) | (1 << WGM21) | (1 << WGM20); // Toggle OC2A on Match
-  TCCR2B = (1 << WGM22) | (1 << CS20);                 // No Prescaler
+  TCCR2A = (1 << COM2A0) | (1 << WGM21) | (1 << WGM20); // toggle OC2A on match
+  TCCR2B = (1 << WGM22) | (1 << CS20);                 // no prescaler
   
   OCR2A = 0;   // Toggle every clock cycle
 }
